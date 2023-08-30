057f160d3f95b300b7e96c05c7cfc0ffbf715a7a
yyj <yanyongjuyyj@outlook.com>
2023-08-30 13:41:59 +0800

    modified:   config.php

--- config.php
+++ config.php
@@ -1,25 +1,52 @@
 <?php
 //配置文件
 
+
+//网站设置
+////////////////////////////////
 //默认靶场路径
 $vulhub_path = '/var/www/html/vulhub';
 
 //默认主机
+//域名或ip
 $ip = "192.168.120.128";
 $port = '80';
+////////////////////////////////
 
+
+//数据库配置
+////////////////////////////////
 //默认数据库名
-$db_name = 'vulhub';
-
+const db_name = 'vulhub';
+//主机名
+const db_host = '127.0.0.1';
+//数据库用户名
+const db_user = 'root';
 //默认数据库密码
-$db_pass = "root";
+const db_pass = "root";
+define('DB_HOST', db_host);
+define('DB_USER', db_user);
+define('DB_PASS', db_pass);
+define('DB_NAME', db_name);
+////////////////////////////////
 
-$path = '';
+
+
+
+//网站文件配置
+/////////////////////////////////
+$web_path = '';
 //默认主页
 $index = "vulhub.php";
 
 //默认开启docker的文件
-$docker = 'docker.php';
+$dockerfile = 'docker.php';
 
 //初始化文件
 $setup = 'setup.php';
+
+
+//网站高级设置
+////////////////////////////////
+//是否开启防御
+$waf = false;
